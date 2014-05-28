d6d37b57570ff469c14bb346e3efbaa4b543d8ce
ayham <ayham.a.alzoubi@gmail.com>
2014-05-28 12:22:08 +0400

    Adding Ip Validate function to functions * Added a function ip_validate to validate if the ip is in valid ips or subnebts * Added the test for ip_validate with all cases * The function accepts $valisIps as ip, array of ips, subnet range or array of subnet ranges

--- tests/Namshi/Utils/Test/FunctionsTest.php
+++ tests/Namshi/Utils/Test/FunctionsTest.php
@@ -50,4 +50,127 @@ class FunctionsTest extends PHPUnit_Framework_TestCase
         $this->assertCount(3, array_get($array, '[keys]'));
         $this->assertEquals('key2', array_get($array, '[keys][1]'));
     }
+
+    public function testValidateIpWithValidIp()
+    {
+        $valid = validate_ip('192.168.100.110', '192.168.100.64/26');
+
+        $this->assertTrue($valid);
+    }
+
+    public function testValidateIpWithValidIpAndArrayOfRanges()
+    {
+        $valid = validate_ip('192.168.100.110', array(
+            '192.168.100.64/26',
+            '10.168.100.64/26',
+            '15.168.100.64/26'
+        ));
+
+        $this->assertTrue($valid);
+    }
+
+    public function testValidateIpWithValidIpAndArrayOfIps()
+    {
+        $valid = validate_ip('192.168.100.110', array(
+            '192.168.100.110',
+            '192.168.100.115',
+            '192.168.100.0'
+        ));
+
+        $this->assertTrue($valid);
+    }
+
+    public function testValidateIpWithValidIpAndArrayOfIpsNotInTheArray()
+    {
+        $valid = validate_ip('192.168.100.110', array(
+            '192.168.100.115',
+            '192.168.100.0'
+        ));
+
+        $this->assertFalse($valid);
+    }
+
+    public function testValidateIpWithValidIpAndEmptyArrayOfIps()
+    {
+        $valid = validate_ip('192.168.100.110', array());
+
+        $this->assertFalse($valid);
+    }
+
+    public function testValidateIpWithValidIpAndTheSameIp()
+    {
+        $valid = validate_ip('192.168.100.110', '192.168.100.110');
+
+        $this->assertTrue($valid);
+    }
+
+    public function testValidateIpWithValidIpAndDifferentIp()
+    {
+        $valid = validate_ip('192.168.100.110', '192.168.100.111');
+
+        $this->assertFalse($valid);
+    }
+
+    public function testValidateIpWithIpNotInTheRange()
+    {
+        $valid = validate_ip('55.192.168.100', '192.168.100.64/26');
+
+        $this->assertFalse($valid);
+    }
+
+    public function testValidateIpWithIpAndObject()
+    {
+        $valid = validate_ip('55.192.168.100', new stdClass());
+
+        $this->assertFalse($valid);
+    }
+
+    public function testValidateIpWithIpAndBoolean()
+    {
+        $valid = validate_ip('55.192.168.100', true);
+
+        $this->assertFalse($valid);
+    }
+
+    public function testValidateIpWithIpAndInteger()
+    {
+        $valid = validate_ip('55.192.168.100', 55);
+
+        $this->assertFalse($valid);
+    }
+
+    public function testValidateIpWithIpAndFloat()
+    {
+        $valid = validate_ip('55.192.168.100', 55.5);
+
+        $this->assertFalse($valid);
+    }
+
+    public function testValidateIpWithValidIpCompleteRangeAnd32Mask()
+    {
+        $valid = validate_ip('192.168.100.15', '0.0.0.0/32');
+
+        $this->assertFalse($valid);
+    }
+
+    public function testValidateIpWithInValidIp()
+    {
+        $valid = validate_ip('Invalid Ip', '192.168.100.64/26');
+
+        $this->assertFalse($valid);
+    }
+
+    public function testValidateIpWithInValidRange()
+    {
+        $valid = validate_ip('192.168.100.110', 'Invalid Range');
+
+        $this->assertFalse($valid);
+    }
+
+    public function testValidateIpWithInValidRangeAndInvalidIp()
+    {
+        $valid = validate_ip('Invalid Ip', 'Invalid Range');
+
+        $this->assertFalse($valid);
+    }
 }
